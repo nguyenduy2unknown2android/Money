0849c30873c81a85331d71db8355880380d68835
nguyenduy2unknown2android <nguyenduy.unknown.android@gmail.com>
2016-04-02 17:07:25 +0100

    02042016

---
+++ app/src/androidTest/java/com/ahcoffee/nguyenduy/ahcoffee/MyUnitTest.java
@@ -0,0 +1,13 @@
+package com.ahcoffee.nguyenduy.ahcoffee;
+
+import org.junit.Test;
+
+/**
+ * Created by nguyen duy on 02/04/2016.
+ */
+public class MyUnitTest {
+    @Test
+    public void testDb() {
+
+    }
+}
